b6cbdf3b980129ed42184edc8cf7a4c758570033
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-09 15:10:44 +0900

    ユーザー登録画面を編集

--- user/index.php
+++ user/index.php
@@ -1,2 +1,42 @@
-<?php
-echo "ユーザー登録ページ!!";
+<!DOCTYPE html>
+<html lang="ja">
+    <?php
+    include_once "../common/header.php";
+    echo getHeader('ユーザー登録');
+    ?>
+<body>
+    <div class="d-flex align-items-center justify-content-center h-100">
+        <form method="post" action="../memo/">
+            <div class="card rounded login-card-width shadow">
+                <div class="card-body">
+                    <div class="rounded-circle mx-auto border-gray border d-flex mt-3 icon-circle">
+                        <img src="../public/images/animal_stand_zou.png" class="w-75 mx-auto p-2" alt="icon"/>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <div class="mt-3 h2">SimpleMemo</div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="offset-2 col-8 offset-2">
+                            <label class="input-group w-100">
+                                <sapn class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-file-signature"></li></span>
+                                </span>
+                                <input type="text" name="user_name" class="form-control" placeholder="ユーザー名" autocomplete="off" maxlength="255" />
+                            </label>
+                            <label class="input-group w-100">
+                                <span class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-key"></li></span>
+                                </span>
+                                <input type="password" name="user_password" class="form-control" placeholder="パスワード" autocomplete="off" maxlength="255" />
+                            </label>
+                            <button type="submit" class="form-control btn btn-success">
+                            登録する
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
