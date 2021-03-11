22cacbcbe4eab1a01c554fe2d12c4eb79a4923aa
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-11 20:53:25 +0900

    ユーザー登録処理にバリデーションを実装

--- user/action/register.php
+++ user/action/register.php
@@ -1,10 +1,42 @@
 <?php
+    session_start();
+    require '../../common/validation.php';
     require '../../common/database.php';
-
     // パラメータ取得
     $user_name = $_POST['user_name'];
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
+
+    //バリデーション
+    $_SESSION['errors'] = [];
+
+    //空チェック
+    emptyCheck($_SESSION['errors'],$user_name,"ユーザー名を入力してください。");
+    emptyCheck($_SESSION['errors'],$user_email,"メールアドレスを入力してください。");
+    emptyCheck($_SESSION['errors'],$user_password,"パスワードを入力してください。");
+
+    // - 文字数チェック
+    stringMaxSizeCheck($_SESSION['errors'],$user_name,"ユーザー名は255文字以内で入力してください。");
+    stringMaxSizeCheck($_SESSION['errors'],$user_email,"メールアドレスは255文字以内で入力してください。");
+    stringMaxSizeCheck($_SESSION['errors'],$user_password,"パスワードは255文字以内で入力してください。");
+    stringMaxSizeCheck($_SESSION['errors'],$user_password,"パスワードは8文字以上で入力してください。");
+
+    if(!$_SESSION['errors']) {
+        //メールアドレスチェック
+        mailAddressCheck($_SESSION['errors'],$user_email,"正しいメールアドレスを入力してください。");
+
+        // - ユーザー名・パスワード半角英数チェック
+        halfAlphanumericCheck($_SESSION['errors'],$user_name,"ユーザー名は半角英数字で入力してください。");
+        halfAlphanumericCheck($_SESSION['errors'],$user_password,"パスワードは半角英数字で入力してください。");
+
+        // - メールアドレス重複チェック
+        mailAddressDuplicationCheck($_SESSION['errors'],$user_email,"既に登録されているメールアドレスです。");
+    }
+
+    if($_SESSION['errors']) {
+        header('Location: ../../user/');
+        exit;
+    }
 
     // DB接続処理
     $database_handler = getDatabaseConnection();
