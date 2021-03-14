35ef6e1bc30e21020f390e301a6804b6bdf74920
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-14 23:25:56 +0900

    エラー解消

--- login/action/login.php
+++ login/action/login.php
@@ -3,7 +3,7 @@
     require '../../common/validation.php';
     require '../../common/database.php';
 
-    //パラメータ取得
+    // パラメータ取得
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
 
