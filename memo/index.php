274718318ab9b4a11df9fd7da21511bef4902e14
澤田大樹 <hiroki.bigtree@icloud.com>
2021-03-17 20:45:58 +0900

    エラー解消

--- memo/index.php
+++ memo/index.php
@@ -45,7 +45,7 @@
                         </div>
                         <div class="pr-1">
                             <a href="./action/add.php" class="btn btn-success"><i class="fas fa-plus"></i></a>
-                            <a href="../login/" class="btn btn-dark"><i class="fas fa-sign-out-alt"></i></a>
+                            <a href="./action/logout.php" class="btn btn-dark"><i class="fas fa-sign-out-alt"></i></a>
                         </div>
                     </div>
                     <div class="left-memo-title h3 pl-3 pt-3">
